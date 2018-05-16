92174c80754774e1cb9e81c9d96bbded25e6b878
Guillermo Gomez <ggomez@claritycon.com>
2018-05-16 11:53:28 -0500

    Initial project structure with webpack and wepack dev server with hot reload

---
+++ build/webpack.config.dev.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: [
+    './src/app.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, "../public"),    
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'build/templates/index.html',
+      inject: true
+    })
+  ]
+}
+
---
+++ src/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Hello Guillermo!</h1>
+  </div>
+</template>
